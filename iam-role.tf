6da3a33e71c4359f7baebb4f54e759e7858f042c
Alexey Trukhlyayev <exact84@gmail.com>
2025-06-14 02:20:17 +0500

    Add Terraform config and IAM role setup

---
+++ iam-role.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GithubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::364777501483:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:exact84/rsschool-devops-course-tasks:*"
+          },
+          "StringEquals": {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_role_policies" {
+  for_each = toset([
+    "AmazonEC2FullAccess",
+    "AmazonRoute53FullAccess",
+    "AmazonS3FullAccess",
+    "IAMFullAccess",
+    "AmazonVPCFullAccess",
+    "AmazonSQSFullAccess",
+    "AmazonEventBridgeFullAccess"
+  ])
+
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/${each.key}"
+}
