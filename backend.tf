60ddc950af58a6b4495b9da7a30d01da6577e3b2
Alexey Trukhlyayev <exact84@gmail.com>
2025-06-24 19:50:18 +0500

    fix: bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "k8s-terraform-state-140484"
     key            = "terraform/k8s-vpc/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
